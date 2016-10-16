d481722a0cfd8b6de9ed16dd3ce112c9bffcdb33
Arkadi-PC\Arkadi <arkoman2@gmail.com>
2016-10-16 15:38:49 +0300

    no message

--- MyApplication2/app/src/main/java/imashelha/pazortower/MainActivity.java
+++ MyApplication2/app/src/main/java/imashelha/pazortower/MainActivity.java
@@ -1,7 +1,13 @@
 package imashelha.pazortower;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("Contacts");
+
+        Button btButton1 = (Button) findViewById(R.id.button1);
+
+        btButton1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
